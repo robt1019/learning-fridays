fb04cd3a9d1bf75c566009bcded9497242bf49cc
Rob Taylor <robert.i.taylor88@gmail.com>
2018-11-23 14:30:48 +0000

    initial commit

---
+++ ab-testing/split-test/lib/redux/reducers/index.js
@@ -0,0 +1,4 @@
+import experiments from './experiments'
+export default {
+  experiments
+}
---
+++ ab-testing/split-test/pages/index.js
@@ -0,0 +1,18 @@
+import initStore from '../lib/initRedux'
+import withRedux from 'next-redux-wrapper'
+
+import {startExperiment} from "../lib/redux/reducers/experiments";
+
+const Index = () => <div>Welcome to next.js!</div>;
+/* context: {req, res, query, isServer, store} */
+Index.getInitialProps = ({store}) => {
+  const dispatchStartExperiment = ({name, variant}) => {
+    store.dispatch(startExperiment({
+      name,
+      variant
+    }))
+  }
+};
+const mapStateToProps = () => ({});
+const mapDispatchToProps = () => ({});
+export default withRedux(initStore, mapStateToProps, mapDispatchToProps)(Index)
